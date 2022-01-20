f016f5c038f0078790eb8e89999395b37e3287d6
josueeegl <josuegl12@outlook.com>
2022-01-19 20:57:26 -0600

    v5

---
+++ assets/js/detectlanguage.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var ln = (x = window.navigator.language || navigator.browserLanguage);
+    if (ln == "es") {
+        window.location.href = "index_ES.html"; // si es es va a español
+    } 
+};
--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,15 @@
 function initMap() {
-    // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
-
+    var coordenadas = {
+        lat: 14.138148814610386,
+        lng: -90.01161422964613
+    };
     var map = new google.maps.Map(document.getElementById('google-maps'), {
-        zoom: 17,
-        center: myLatLng
+        zoom: 10,
+        center: coordenadas
     });
 
     var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'South Jakarta, INA' // Title Location
+        position: coordenadas,
+        map: map
     });
 }

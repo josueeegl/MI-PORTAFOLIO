b2649dbb8b4ac1952bee89a9e4073c220be98e0b
josueeegl <josuegl12@outlook.com>
2022-01-20 10:24:42 -0600

    v6

--- assets/js/detectlanguage.js
+++ assets/js/detectlanguage.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    
     var ln = (x = window.navigator.language || navigator.browserLanguage);
     if (ln == "es") {
         window.location.href = "index_ES.html"; // si es es va a español

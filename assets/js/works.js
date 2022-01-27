3c7f0dac6f4cbcaf8d225484b462a677dbed02c9
josueeegl <josuegl12@outlook.com>
2022-01-27 10:36:06 -0600

    1.06

--- assets/js/works.js
+++ assets/js/works.js
@@ -1,10 +1,33 @@
-const grid = new Muuri('.gridWork', {
-    layout: {
-        rounding: false
+const overlay = document.getElementById('gridWork-overlay');
+document.querySelectorAll('.gridWork .gridWork-title').forEach((element) => {
+
+    element.addEventListener('click', () => {
+        const ruta = element.querySelector('img').getAttribute('src');
+        const description = element.querySelector('p').innerHTML;
+        overlay.classList.add('activo');
+        document.querySelector('#gridWork-overlay img').src = ruta;
+        document.querySelector('#gridWork-overlay .gridWork-descripcion').innerHTML = description;
+    })
+
+});
+
+document.getElementById('btn-cerrar-popup').addEventListener('click', () => {
+    overlay.classList.remove('activo');
+});
+
+overlay.addEventListener('click', (event) => {
+    if(event.target.id === 'gridWork-overlay'){
+        overlay.classList.remove('activo');
+        document.querySelector('#gridWork-overlay img').style.transform = 'scale(1)';
     }
 });
 
-window.addEventListener('load', () => {
-    grid.refreshItems().layout();
-    document.getElementById('gridWork').classList.add('imagenes-cargadas');
+document.querySelector('#gridWork-overlay img').addEventListener('click', () => {
+    var t = document.querySelector('#gridWork-overlay img');
+    if (t.style.transform == 'scale(1.9)') {
+        t.style.transform = 'scale(1)';
+    } else {
+        t.style.transform = 'scale(1.9)';
+    }
+
 });
